8cc5cd2b628b984f4ef26502accc3692d61bef28
zhumengcheng <944989685@qq.com>
2021-09-13 10:59:43 +0800

    题解

---
+++ 栈,DFS/第四天.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+    let num = 0;
+    let numStock = []
+    let resultStock = []
+    let result = ''
+    for (let str in s) {
+        if (!isNaN(s[str])) {
+            num = num * 10 + Number(s[str])
+        } else if (s[str] == '[') {
+            resultStock.push(result);
+            result = '';
+            numStock.push(num)
+            num = 0;
+        } else if (s[str] == ']') {
+            let copyNum = numStock.pop();
+            result = resultStock.pop() + result.repeat(copyNum);
+        } else {
+            result += s[str]
+        }
+    }
+    return result
+
+};
+
+console.log(decodeString('3[a]2[bc]'))
