181306f477b3006db2ed230bbb5c00d5f619ff2a
zhumengcheng <944989685@qq.com>
2021-10-11 16:19:57 +0800

    删除排序数组中的重复项题解思路

--- 哈希表/1011-删除排序数组中的重复项题解思路.js
+++ 哈希表/1011-删除排序数组中的重复项题解思路.js
@@ -5,7 +5,7 @@ const arr = [1, 2, 2, 3, 4, 4, 4, 4, 4, 5, 6, 7, 9, 9]
  * 双指针 第一个记录有效数组长度,第二个遍历数组所有长度
  */
 function deleteSortNum(arr) {
-    let firstIndex = 0;
+    let firstIndex = 0; //指向前面的有效数组
     for (let secondIndex = 1; secondIndex < arr.length; secondIndex++) {
         if (arr[firstIndex] != arr[secondIndex]) {
             arr[firstIndex + 1] = arr[secondIndex]
@@ -15,6 +15,6 @@ function deleteSortNum(arr) {
     return firstIndex + 1
 }
 
+时间复杂度: O(N)
+空间复杂度: O(1)
 console.log(deleteSortNum(arr)) // 8
-// 时间复杂度: O(N)
-// 空间复杂度: O(1)
